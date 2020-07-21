028b2acf1db007eadcfd850925e2270697773f9d
matsuokanao <matsuokanao1029@gmail.com>
2020-07-21 20:11:08 +0900

    firstpush

---
+++ RelayApplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RelayApplication
+//
+//  Created by 松岡奈央 on 2020/07/21.
+//  Copyright © 2020 松岡奈央. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
